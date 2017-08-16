5920ca7f63d73f68a725d4a8b1d0cd90c7049564
wileyj <wileyj@users.noreply.github.com>
2017-08-15 23:19:10 -0700

    updates

--- convert/testing/audio.py
+++ convert/testing/audio.py
@@ -1,4 +1,8 @@
 import re
+import logging
+
+log_format = '[%(levelname)-s] %(filename)-s %(funcName)-4s(%(lineno)-s) - %(message)-4s'
+logging.basicConfig(level=logging.INFO, format=log_format)
 
 
 class HandBrakeAudioInfo(object):
@@ -7,43 +11,47 @@ class HandBrakeAudioInfo(object):
 
     def __init__(self, info_str):
         match = self.pattern1.match(info_str)
-        print "Info_str: %s" % (info_str)
-        print "Match: %s" % (match)
+        logging.info("Info_str: %s" % (info_str))
+        logging.info("Match pattern1: %s" % (match))
         if not match:
             raise ValueError("Unknown audio track info format: " + repr(info_str))
-        print "Line 12"
+        logging.info("")
         self.index = int(match.group(1))
         self.description = match.group(2)
         self.language_code = match.group(3)
+        logging.info("match1 index:: %s" % (self.index))
+        logging.info("match1 description:: %s" % (self.description))
+        logging.info("match1 language_code:: %s" % (self.language_code))
         match = self.pattern2.match(info_str)
-        print "Line 17"
+        logging.info("Match pattern2: %s" % (match))
+        logging.info("")
         if match:
-            print "Line 19"
+            logging.info("match:: %s" % (match))
             self.sample_rate = int(match.group(4))
             self.bit_rate = int(match.group(5))
         else:
-            print "Line 23"
+            logging.info("not match:: %s" % (match))
             self.sample_rate = None
             self.bit_rate = None
         self.title = None
 
     def __str__(self):
-        print "Line 29"
+        logging.info("line:: ")
         format_str = (
             "Description: {description}\n"
             "Language code: {language_code}"
         )
-        print "Line 34"
+        logging.info("line:: ")
         if self.sample_rate:
-            print "Line 36"
+            logging.info("sample_rate:: ")
             format_str += "\nSample rate: {sample_rate}Hz"
         if self.bit_rate:
-            print "Line 39"
+            logging.info("bit_rate:: ")
             format_str += "\nBit rate: {bit_rate}bps"
         return format_str.format(**self.__dict__)
 
     def __hash__(self):
-        print "Line 44"
+        logging.info("hash:: ")
         return hash((
             self.index,
             self.description,
@@ -54,11 +62,11 @@ class HandBrakeAudioInfo(object):
         ))
 
     def __eq__(self, other):
-        print "Line 55"
+        logging.info("")
         if not isinstance(other, HandBrakeAudioInfo):
-            print "Line 57"
+            logging.info("")
             return False
-        print "Line 59"
+        logging.info("")
         return (
             self.index == other.index and
             self.description == other.description and
