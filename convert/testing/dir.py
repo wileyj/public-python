974aee3659e35a64a78e4e2113695424314562d7
New Name <wileyj@users.noreply.github.com>
2017-08-15 23:19:10 -0700

    updates

--- convert/testing/dir.py
+++ convert/testing/dir.py
@@ -2,18 +2,26 @@ from os import walk
 import os
 import subprocess
 from handbrake import HandBrake
-
+import logging
 
 # move list to a dict, with a boolean for each item. ex: 'avi': True
 # check as if formats[file extension]: blah
 formats = [
     "mkv",
     "avi",
-    "AVI"
+    "AVI",
+    "mp4",
+    "MOV",
+    "m4v",
+    "mov"
 ]
 
-mypath = "/Users/wileyj/Git/wileyj/public-python/convert/testing/files"
+# mypath = "/Users/wileyj/Git/wileyj/public-python/convert/testing/files"
+# mypath = "/Volumes/Media/Music/iTunes/iTunes Media/Home Videos/"
+mypath = "/Volumes/wileyj/Desktop/To Convert"
 handbrake_exe = HandBrake().find_handbrake_executable_in_path("HandBrakeCLI")
+log_format = '[%(levelname)-s] %(filename)-s %(funcName)-4s(%(lineno)-s) - %(message)-4s'
+logging.basicConfig(level=logging.INFO, format=log_format)
 
 found = []
 for (dirpath, dirnames, filenames) in walk(mypath):
@@ -34,14 +42,18 @@ class MatchFile():
 count = 0
 for item in found:
     if MatchFile().format(item):
-        newfile = os.path.splitext(item)[0]
-        print "Supported file: %s :: %s" % (item, newfile)
         count = count + 1
+        newfile = os.path.splitext(item)[0]
+        logging.info("%i) Supported file: %s :: %s" % (count, item, newfile))
+        HandBrake().parse_scan_output(HandBrake().scan_file(handbrake_exe, mypath + "/" + item))
+        audio_tracks = HandBrake().scan_file(handbrake_exe, mypath + "/" + item)
+        logging.info("Audio Tracks: %s" % (audio_tracks))
+        # logging.info("subtitle_tracks: %s" % (subtitle_tracks))
 
-print "matched: %i" % (count)
-print "total: %i" % (len(found))
-print "handbrake_exe: %s" % (handbrake_exe)
-HandBrake().parse_scan_output(HandBrake().scan_file(handbrake_exe, mypath + "/001-sample.avi"))
+logging.info("matched: %i" % (count))
+logging.info("handbrake_exe: %s" % (handbrake_exe))
+# HandBrake().parse_scan_output(HandBrake().scan_file(handbrake_exe, mypath + "/001-sample.avi"))
+# HandBrake().parse_scan_output(HandBrake().scan_file(handbrake_exe, "/Volumes/Media/Music/iTunes/iTunes Media/Home Videos/Total Body - Yoga Fix.m4v"))
 # audio_tracks, subtitle_tracks = HandBrake().scan_file(handbrake_exe, mypath + "/001-sample.avi")
-# print "Audio Tracks: %s" % (audio_tracks)
-# print "subtitle_tracks: %s" % (subtitle_tracks)
+# logging.info("Audio Tracks: %s" % (audio_tracks))
+# logging.info("subtitle_tracks: %s" % (subtitle_tracks))
--- convert/testing/handbrake.py
+++ convert/testing/handbrake.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 from audio import HandBrakeAudioInfo
 
+log_format = '[%(levelname)-s] %(filename)-s %(funcName)-4s(%(lineno)-s) - %(message)-4s'
+logging.basicConfig(level=logging.INFO, format=log_format)
+
 
 class FFmpegStreamInfo(object):
     def __init__(self, stream_index, codec_type, codec_name, language_code, metadata):
@@ -14,6 +17,43 @@ class FFmpegStreamInfo(object):
 
 
 class HandBrake():
+    def parse_handbrake_scan_output(output):
+        lines = output.splitlines()
+        hb_audio_tracks = None
+        hb_subtitle_tracks = None
+        ff_audio_streams = None
+        ff_subtitle_streams = None
+        i = 0
+        while i < len(lines):
+            if lines[i].startswith("Input #0, "):
+                logging.debug("Found FFmpeg stream info")
+                i, ff_audio_streams, ff_subtitle_streams = HandBrake().parse_ffmpeg_stream_info(lines, i)
+                message_format = "FFmpeg: %d audio track(s), %d subtitle track(s)"
+                logging.debug(message_format, len(ff_audio_streams), len(ff_subtitle_streams))
+                continue
+            if lines[i] == "  + audio tracks:":
+                logging.debug("Found HandBrake audio track info")
+                i, hb_audio_tracks = HandBrake().parse_handbrake_track_info(lines, i, HandBrakeAudioInfo)
+                logging.debug("HandBrake: %d audio track(s)", len(hb_audio_tracks))
+                continue
+            if lines[i] == "  + subtitle tracks:":
+                logging.debug("Found HandBrake subtitle track info")
+                i, hb_subtitle_tracks = HandBrake().parse_handbrake_track_info(lines, i, HandBrake().HandBrakeSubtitleInfo)
+                logging.debug("HandBrake: %d subtitle track(s)", len(hb_subtitle_tracks))
+                continue
+            i += 1
+        HandBrake().merge_track_titles(hb_audio_tracks, ff_audio_streams)
+        HandBrake().merge_track_titles(hb_subtitle_tracks, ff_subtitle_streams)
+        return (hb_audio_tracks, hb_subtitle_tracks)
+
+    def merge_track_titles(hb_tracks, ff_streams):
+        if not ff_streams:
+            return
+        assert len(hb_tracks) == len(ff_streams), "Track count mismatch"
+        for hb_track, ff_stream in zip(hb_tracks, ff_streams):
+            assert hb_track.language_code == ff_stream.language_code, "Track language code mismatch"
+            hb_track.title = ff_stream.metadata.get("title")
+
     def parse_handbrake_track_info(self, output_lines, start_index, info_cls):
         prefix = "    + "
         prefix_len = len(prefix)
@@ -31,17 +71,17 @@ class HandBrake():
         i = 0
         while i < len(lines):
             if lines[i] == "  + audio tracks:":
-                logging.critical("Found HandBrake audio track info")
-                logging.critical("Line: %s" % (lines[i]))
+                logging.info("Found HandBrake audio track info")
+                logging.info("Line: %s" % (lines[i]))
                 i, hb_audio_tracks = HandBrake().parse_handbrake_track_info(lines, i, HandBrakeAudioInfo)
-                logging.debug("HandBrake: %d audio track(s)", len(hb_audio_tracks))
+                logging.info("HandBrake: %d audio track(s)", len(hb_audio_tracks))
                 # continue
             i += 1
         return True
 
     def scan_file(self, handbrake_exe, filename):
-        logging.critical("Using Handbrake exe: %s" % (handbrake_exe))
-        logging.critical("Checking on file: %s" % (filename))
+        logging.info("Using Handbrake exe: %s" % (handbrake_exe))
+        logging.info("Checking on file: %s" % (filename))
         output = subprocess.check_output([
             handbrake_exe,
             "-i", filename,
@@ -55,7 +95,7 @@ class HandBrake():
         message_format = "Found HandBrakeCLI binary at '%s'"
         if not os.access(file_path, os.X_OK):
             message_format += ", but it is not executable"
-            logging.warning(message_format, file_path)
+            logging.info(message_format, file_path)
             return False
         logging.info(message_format, file_path)
         return True
